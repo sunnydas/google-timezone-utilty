867b961c98c1dbb5b8bb624ec2cd8d5685138452
Sunny Das <sundas@oradev.oraclecorp.com>
2016-02-15 20:22:49 +0530

    first version of checkins

---
+++ src/util/file/util/FileUtil.java
@@ -0,0 +1,55 @@
+package util.file.util;
+
+import com.sunny.trial.parser.parser.dao.CSVRow;
+
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.List;
+
+/**
+ * Created by SUND on 05-03-2015.
+ */
+public class FileUtil {
+
+    /**
+     *
+     * @param fileName
+     * @param csvRows
+     */
+    public static void writeToFile(String fileName,List<CSVRow> csvRows) throws IOException {
+        File file = new File(fileName);
+        BufferedWriter bufferedWriter = null;
+        try {
+            FileWriter fileWriter = new FileWriter(file);
+            bufferedWriter = new BufferedWriter(fileWriter);
+            StringBuilder stringBuilder = new StringBuilder();
+            for(int i = 0 ; i < csvRows.size();i++){
+              CSVRow csvRow = csvRows.get(i);
+              if(csvRow != null){
+                 stringBuilder.append(csvRow.getUtcDateTime());
+                 stringBuilder.append(",");
+                 stringBuilder.append(csvRow.getLatitude());
+                 stringBuilder.append(",");
+                 stringBuilder.append(csvRow.getLongitude());
+                 stringBuilder.append(",");
+                 stringBuilder.append(csvRow.getLocalizedTimeZone());
+                 stringBuilder.append(",");
+                 stringBuilder.append(csvRow.getLocalizedTime());
+                 if(i < (csvRows.size() - 1)) {
+                     stringBuilder.append(System.getProperty("line.separator"));
+                 }
+              }
+            }
+            bufferedWriter.write(stringBuilder.toString());
+            bufferedWriter.flush();
+        } finally {
+            if(bufferedWriter != null){
+              bufferedWriter.close();
+            }
+        }
+    }
+
+
+}
---
+++ src/util/time/TimeUtilities.java
@@ -0,0 +1,110 @@
+package util.time;
+
+import com.sunny.trial.parser.parser.dao.CSVRow;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.TimeZone;
+
+/**
+ * Created by SUND on 04-03-2015.
+ */
+public class TimeUtilities {
+
+
+    private static final String timezoneBaseURL = "https://maps.googleapis.com/maps/api/timezone/json";
+
+
+    /**
+     *
+     * @param csvRow
+     * @throws IOException
+     */
+    public static void updateLocalizedTimeZone(CSVRow csvRow) throws IOException {
+        String clientID = System.getProperty("googleapiclientid");
+        String latitude = csvRow.getLatitude();
+        String longitude = csvRow.getLongitude();
+        String timeStamp = getTimeStamp(csvRow.getUtcDateTime());
+        StringBuilder restURL = new StringBuilder(timezoneBaseURL);
+        restURL.append("?location=").append(latitude).append(",").append(longitude).append("&timestamp=").append(timeStamp).append("&key=").append(clientID);
+        //System.out.println(restURL.toString());
+        String response = getRESTResponse(restURL.toString());
+        //System.out.println(" response = " + response);
+        JSONParser jsonParser = new JSONParser();
+        Object parsed = null;
+        try {
+            parsed = jsonParser.parse(response);
+        } catch (org.json.simple.parser.ParseException e) {
+            e.printStackTrace();
+        }
+        JSONObject jsonObject = (JSONObject) parsed;
+        if(parsed != null){
+           String tzId = (String) ((JSONObject) parsed).get("timeZoneId");
+           csvRow.setLocalizedTimeZone(tzId);
+        }
+    }
+
+
+    /**
+     *
+     * @param csvRow
+     *
+     */
+    public static void updateLocalizedTime(CSVRow csvRow) throws ParseException {
+        SimpleDateFormat sdf = new SimpleDateFormat("yyyy-M-dd hh:mm:ss");
+        String time = csvRow.getUtcDateTime();
+        sdf.setTimeZone(TimeZone.getTimeZone(csvRow.getLocalizedTimeZone()));
+        Date date = sdf.parse(time);
+        csvRow.setLocalizedTime(sdf.format(date));
+    }
+
+
+    public static String getRESTResponse(String restURL) throws IOException {
+        StringBuilder responseString = new StringBuilder();
+        HttpClient client = new DefaultHttpClient();
+        HttpGet request = new HttpGet(restURL);
+        org.apache.http.HttpResponse response = null;
+        response = client.execute(request);
+        BufferedReader rd = null;
+        rd = new BufferedReader(new InputStreamReader(response.getEntity().getContent()));
+        String line = null;
+        try {
+            while ((line = rd.readLine()) != null) {
+                responseString.append(line);
+            }
+        } finally {
+            if(rd != null){
+                rd.close();
+            }
+        }
+        return responseString.toString();
+    }
+
+
+
+    public static String getTimeStamp(String dateTime) {
+        SimpleDateFormat sdf = new SimpleDateFormat("yyyy-M-dd hh:mm:ss");
+        Date date = null;
+        try {
+            date = sdf.parse(dateTime);
+        } catch (ParseException e) {
+            //ignore and print
+            e.printStackTrace();
+        }
+
+        Calendar calendar = Calendar.getInstance();
+        calendar.setTime(date);
+        return ""+(calendar.getTimeInMillis()/1000);
+    }
+
+}
